1a8191fb992dfbcab3e49835ff1f606caa2775b9
Artur Amantaev <aa@megaperfume.ru>
2023-11-24 20:15:52 +0300

    task4

---
+++ task4/vite.config.js
@@ -0,0 +1,32 @@
+export default {
+  outDir: 'dist',
+
+  base: '/',
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+      },
+    },
+    include: ['style.css']
+  },
+
+  server: {
+    port: 3000,
+    open: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    lib: false,
+    rollupOptions: {
+    },
+  },
+};
